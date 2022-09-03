c8986bc8ed2e974cc8dd113f15562de9b986f091
RodMaker <83619630+RodMaker@users.noreply.github.com>
2022-09-03 12:50:41 +0100

    initial commit

---
+++ Assets/Scripts/CarController.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarController : MonoBehaviour
+{
+    // get input
+    // use input to move sphere
+
+    private float moveInput;
+    private float turnInput;
+    private bool isCarGrounded;
+
+    public float airDrag;
+    public float groundDrag;
+
+    public float fwdSpeed;
+    public float revSpeed;
+    public float turnSpeed;
+    public LayerMask groundLayer;
+
+    public Rigidbody sphereRB;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // detach rigidbody from car
+        sphereRB.transform.parent = null;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        moveInput = Input.GetAxisRaw("Vertical");
+        turnInput = Input.GetAxisRaw("Horizontal");
+
+        // adjust speed for car
+        moveInput *= moveInput > 0 ? fwdSpeed : revSpeed;
+
+        // set cars position to sphere
+        transform.position = sphereRB.transform.position;
+
+        // set cars rotation
+        float newRotation = turnInput * turnSpeed * Time.deltaTime * Input.GetAxisRaw("Vertical");
+        transform.Rotate(0, newRotation, 0, Space.World);
+
+        // raycast ground check
+        RaycastHit hit;
+        isCarGrounded = Physics.Raycast(transform.position, -transform.up, out hit, 1f, groundLayer);
+
+        // rotate car to be parallel to ground
+        transform.rotation = Quaternion.FromToRotation(transform.up, hit.normal) * transform.rotation;
+    
+        if (isCarGrounded)
+        {
+            sphereRB.drag = groundDrag;
+        }
+        else
+        {
+            sphereRB.drag = airDrag;
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        if (isCarGrounded)
+        {
+            // move car
+            sphereRB.AddForce(transform.forward * moveInput, ForceMode.Acceleration);
+        }
+        else
+        {
+            // add extra gravity
+            sphereRB.AddForce(transform.up * -30f);
+        }
+        
+    }
+}
